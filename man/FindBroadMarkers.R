d2071520e52ec33fd3e76ecc4a5bb93875e69898
jjyeh-unc <52042420+jjyeh-unc@users.noreply.github.com>
2026-04-24 10:26:24 -0400

    add FindSubtypeMarkers and FindBroadMarkers

---
+++ man/FindBroadMarkers.R
@@ -0,0 +1,94 @@
+#' Find highly specific marker genes for all clusters in a \code{Seurat} object.
+#'
+#' @name FindBroadMarkers
+#' @author Jack Leary
+#' @description This function finds marker genes for all clusters, and then filters those markers on a per-cluster basis against the most highly expressed genes in other clusters.
+#' @import magrittr
+#' @importFrom SeuratObject GetAssayData
+#' @importFrom Matrix t
+#' @importFrom dplyr mutate group_by summarise across filter pull bind_rows
+#' @importFrom Seurat FindAllMarkers
+#' @importFrom stats quantile
+#' @param seurat.object The \code{Seurat} object containing clusters for which you'd like marker genes identified. Defaults to NULL.
+#' @param assay.use The assay to use when testing. Defaults to "RNA".
+#' @param slot.use The matrix to use when testing. Defaults to "data", for normalized counts, but "counts" (for the raw counts) can also be used.
+#' @param ident.use The cell identity to group by. Defaults to "seurat_clusters".
+#' @param de.method The differential expression method used in \code{\link[Seurat]{FindAllMarkers}}. Defaults to "wilcox".
+#' @param perc.cutoff The percentile cutoff used to find highly expressed genes in other cluster. Defaults to 0.9.
+#' @param log2fc.cutoff The log2FC cutoff used, in part, to determine whether a gene is differentially expressed. Defaults to 0.25.
+#' @param fdr.cutoff The cutoff used to remove DE genes with non-significant adjusted \emph{p}-values. Defaults to 0.05.
+#' @return A data.frame of markers for each cluster filtered by genes considered highly expressed in other clusters.
+#' @seealso \code{\link[Seurat]{FindAllMarkers}}
+#' @export
+#' @examples
+#' \dontrun{
+#' FindSpecificMarkers(seurat_object,
+#'                     method = "wilcox",
+#'                     ident.use = "celltype")
+#' FindSpecificMarkers(seurat_object,
+#'                     method = "wilcox",
+#'                     assay.use = "SCT",
+#'                     slot.use = "data")
+#' }
+FindBroadMarkers <- function(seurat.object = NULL,
+                                assay.use = "RNA",
+                                slot.use = "data",
+                                ident.use = "seurat_clusters",
+                                de.method = "wilcox",
+                                perc.cutoff = 0.9,
+                                log2fc.cutoff = 0.25,
+                                fdr.cutoff = 0.05,
+                                min.pct = 0.25) {
+  # check inputs
+  if (is.null(seurat.object)) { stop("You forgot to provide a Seurat object!") }
+  if (!ident.use %in% colnames(seurat.object@meta.data)) { stop("ident.use must exist in the object's metadata.") }
+  Idents(seurat.object) <- ident.use
+  # get highly expressed genes for each cluster
+  gene_means_by_clust <- Matrix::t(SeuratObject::GetAssayData(seurat.object, assay = assay.use, slot = slot.use)) %>%
+    as.data.frame() %>%
+    dplyr::mutate(cell_ident = unname(unlist(seurat.object[[ident.use]]))) %>%
+    dplyr::group_by(cell_ident) %>%
+    dplyr::summarise(dplyr::across(where(is.numeric), mean))
+  # find list of genes w/ mean expression above 90th percentile of expression in each celltype
+  high_exp_genes <- c()
+  cluster_labels <- c()
+  for (i in gene_means_by_clust$cell_ident) {
+    top_exp_genes <- gene_means_by_clust %>%
+      dplyr::filter(cell_ident == i) %>%
+      dplyr::select(-cell_ident) %>%
+      t() %>%
+      as.data.frame() %>%
+      dplyr::filter(V1 > stats::quantile(V1, perc.cutoff)) %>%
+      dplyr::mutate(gene = rownames(.)) %>%
+      dplyr::pull(gene)
+    high_exp_genes <- c(high_exp_genes, top_exp_genes)
+    cluster_labels <- c(cluster_labels, rep(i, length(top_exp_genes)))
+  }
+  high_exp_gene_df <- data.frame(High_Exp_Genes = high_exp_genes,
+                                 Cluster = as.factor(cluster_labels))
+  # get marker genes w/ normal method
+  marker_genes <- Seurat::FindAllMarkers(seurat.object,
+                                         assay = assay.use,
+                                         slot = slot.use,
+                                         logfc.threshold = log2fc.cutoff,
+                                         test.use = de.method,
+                                         only.pos = TRUE,
+                                         verbose = FALSE,
+                                         min.pct = min.pct,
+                                         random.seed = 312) %>%
+    dplyr::filter(p_val_adj < fdr.cutoff)
+  # remove highly expressed genes in other clusters from each cluster's markers
+  specific_marker_genes <- NULL
+  for (i in unique(marker_genes$cluster)) {
+    outgroup_high_exp_genes <- high_exp_gene_df %>%
+      dplyr::filter(Cluster != i) %>%
+      dplyr::pull(High_Exp_Genes) %>%
+      unique()
+    sub_df <- dplyr::filter(marker_genes,
+                            cluster == i,
+                            !(gene %in% outgroup_high_exp_genes))
+    specific_marker_genes <- specific_marker_genes %>%
+      dplyr::bind_rows(sub_df)
+  }
+  return(specific_marker_genes)
+}
---
+++ man/FindSubtypeMarkers.R
@@ -0,0 +1,91 @@
+#' Find marker genes for a given set of cells, and filter out highly expressed genes from cells in a reference Seurat object.
+#'
+#' @name FindSubtypeMarkers
+#' @author Jack Leary
+#' @description Used to filter candidate marker genes for a subcluster Seurat object. Prevents markers from being chosen if they are highly expressed in other celltypes.
+#' @import magrittr
+#' @importFrom Seurat Idents DefaultAssay FindAllMarkers
+#' @importFrom dplyr filter mutate group_by summarise across ungroup select pull
+#' @importFrom stats quantile
+#' @param obj.1 The object containing clusters that you want marker genes for. Defaults to NULL.
+#' @param obj.2 The object containing reference data against which we'll filter the markers in \code{obj.1}. Defaults to NULL.
+#' @param ident.1 The group identifier for \code{obj.1}. Defaults to "seurat_clusters".
+#' @param ident.2 The group identifier for \code{obj.2}. Defaults to "seurat_clusters".
+#' @param de.method The differential expression method used in \code{\link[Seurat]{FindAllMarkers}}. Defaults to "wilcox".
+#' @param fdr.cutoff The cutoff used to remove DE genes with non-significant adjusted \emph{p}-values. Defaults to 0.05.
+#' @param log2fc.cutoff The log2FC cutoff used, in part, to determine whether a gene is differentially expressed. Defaults to 0.25.
+#' @param perc.cutoff The percentile cutoff used to find highly expressed genes in other cluster. Defaults to 0.9.
+#' @param extra.cell.filter An optional list of extra cells to filter out of \code{obj.2} other than the cells in \code{obj.1}. Defaults to NULL.
+#' @return A data.frame of markers for each cluster in \code{obj.1} filtered by genes considered highly expressed in clusters in \code{obj.2}.
+#' @seealso \code{\link{FindSpecificMarkers}}
+#' @seealso \code{\link[Seurat]{FindAllMarkers}}
+#' @export
+#' @examples
+#' \dontrun{
+#' FindFilteredMarkers(obj.1 = subclust_obj,
+#'                     obj.2 = full_obj,
+#'                     ident.1 = "label",
+#'                     ident.2 = "seurat_clusters")
+#' FindFilteredMarkers(obj.1 = subclust_obj,
+#'                     obj.2 = full_obj,
+#'                     de.method = "t",
+#'                     fdr.cutoff = 0.01,
+#'                     log2fc.cutoff = 1,
+#'                     perc.cutoff = 0.95)
+#' }
+
+
+FindSubtypeMarkers <- function(obj.1 = NULL,
+                                obj.2 = NULL,
+                                ident.1 = "seurat_clusters",
+                                ident.2 = "seurat_clusters",
+                                de.method = "wilcox",
+                                fdr.cutoff = 0.05,
+                                log2fc.cutoff = 0.25,
+                                perc.cutoff = 0.9,
+                                min.pct = 0.25,
+                                extra.cell.filter = NULL) {
+  # check inputs
+  if (is.null(obj.1) || is.null(obj.2)) { stop("You must provide two non-NULL Seurat objects to FindFilteredMarkers().") }
+  if (!ident.1 %in% colnames(obj.1@meta.data)) { stop("ident.1 not in obj.1 metadata.") }
+  if (!ident.2 %in% colnames(obj.2@meta.data)) { stop("ident.2 not in obj.2 metadata.") }
+  Seurat::Idents(obj.1) <- ident.1
+  Seurat::Idents(obj.2) <- ident.2
+  Seurat::DefaultAssay(obj.1) <- "SCT"
+  Seurat::DefaultAssay(obj.2) <- "SCT"
+  markers <- Seurat::FindAllMarkers(obj.1,
+                                    logfc.threshold = log2fc.cutoff,
+                                    test.use = de.method,
+                                    only.pos = TRUE,
+                                    verbose = FALSE,
+                                    min.pct = min.pct,
+                                    random.seed = 312) %>%
+    dplyr::filter(p_val_adj < fdr.cutoff)
+  # filter obj.1 cells from obj.2
+  obj.2 <- obj.2[, !rownames(obj.2@meta.data) %in% rownames(obj.1@meta.data)]
+  if (!is.null(extra.cell.filter)) {
+    obj.2 <- obj.2[, !rownames(obj.2@meta.data) %in% extra.cell.filter]
+  }
+  gene_means_by_clust <- t(obj.2@assays$SCT@data) %>%
+    as.data.frame() %>%
+    dplyr::mutate(label_fine = obj.2[[ident.2]][, 1]) %>%
+    dplyr::group_by(label_fine) %>%
+    dplyr::summarise(dplyr::across(where(is.numeric), mean)) %>%
+    dplyr::ungroup()
+  high_exp_genes <- c()
+  loop_celltypes <- unique(obj.2[[ident.2]][, 1])
+  for (i in loop_celltypes) {
+    top_exp_genes <- gene_means_by_clust %>%
+      dplyr::filter(label_fine == i) %>%
+      dplyr::select(-label_fine) %>%
+      t() %>%
+      as.data.frame() %>%
+      dplyr::filter(V1 > stats::quantile(V1, perc.cutoff)) %>%
+      dplyr::mutate(gene = rownames(.)) %>%
+      dplyr::pull(gene)
+    high_exp_genes <- c(high_exp_genes, top_exp_genes)
+  }
+  high_exp_genes <- unique(high_exp_genes)
+  markers %<>% dplyr::filter(!gene %in% high_exp_genes)
+  return(markers)
+}
